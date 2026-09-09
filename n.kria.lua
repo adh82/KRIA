02ba844ec1797e5bb6651ef4585eef7876a1f5c6
adh82 <adh82@users.noreply.github.com>
2026-09-09 18:36:28 +1000

    Synchronize Loop Sync all track lengths

--- n.kria.lua
+++ n.kria.lua
@@ -347,13 +347,18 @@ function get_step_index(x, track)
 end
 
 function sync_step_count(track)
-	local count = get_step_count(track)
+	local count = get_step_count(track or 1)
 	local max_page = math.max(1, math.ceil(count / 16))
 	if data:get_global_val('step_page') > max_page then
 		data:set_global_val('step_page', max_page)
 	end
-	for _, page in ipairs(pages_with_steps) do
-		data:set_page_val(track or at(), page, 'loop_last', count)
+	local first_track = track or 1
+	local last_track = track or NUM_TRACKS
+	for t = first_track, last_track do
+		local track_count = data:get_global_val('loop_sync') == 2 and count or get_step_count(t)
+		for _, page in ipairs(pages_with_steps) do
+			data:set_page_val(t, page, 'loop_last', track_count)
+		end
 	end
 end
 
